072b0852981ed07049026fc97a1e663a35946cb8
caslavn <caslav.nedeljkovic@dev.telnet.rs>
2021-12-17 15:26:21 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringSecurityOAuth2andJWT/SpringSecurityOAuth2andJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityOAuth2andJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2andJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2andJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSecurityOAuth2andJWT/SpringSecurityOAuth2andJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityOAuth2andJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2andJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
